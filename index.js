93bbb96493fc31a00df5b335be99f877051fd1a5
olight0 <hussin.20062009@gmail.com>
2025-06-26 18:14:50 +0300

    Loading screen add for the website.

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const elements = document.querySelectorAll("h2, h4");
+document.addEventListener("DOMContentLoaded", function () {
+  const elements = document.querySelectorAll("h2, h4");
+  let currentDirection = Math.random() < 0.5 ? "Left" : "Right";
 
-let currentDirection = Math.random() < 0.5 ? "Left" : "Right";
+  function applyAnimation(direction) {
+    elements.forEach((el) => {
+      el.style.animation = "none";
+      void el.offsetWidth;
+      el.style.animation = `textShine${direction} 4s linear infinite`;
+    });
+  }
 
-function applyAnimation(direction) {
-  elements.forEach(el => {
-    el.style.animation = "none";
-    void el.offsetWidth;
-    el.style.animation = `textShine${direction} 4s linear infinite`;
-  });
-}
-
-applyAnimation(currentDirection);
-
-setInterval(() => {
-  currentDirection = currentDirection === "Left" ? "Right" : "Left";
   applyAnimation(currentDirection);
-}, 4000);
 
+  setInterval(() => {
+    currentDirection = currentDirection === "Left" ? "Right" : "Left";
+    applyAnimation(currentDirection);
+  }, 4000);
+
+  const loadingScreen = document.querySelector(".loadingScreen");
+
+  window.addEventListener("load", function () {
+    loadingScreen.style.display = "none";
+  });
+});
